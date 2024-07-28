fbceefa81c14cc1b2ca5e111a1202d54e00f03f8
Matt <ammn2000@gmail.com>
2024-07-27 20:54:30 -0400

    completed exercises 1 - 5

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -17,11 +17,14 @@ public class Exercise_05 {
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
-
+        System.out.println(str.equals(str2));
         // please concatenate str & str2 and set the result to a new String variable below
-
+        System.out.println(str + str2);
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        System.out.println(str.replace('h', 'm'));
+        System.out.println(str.substring(4));
+        System.out.println(str.replace('h', 'm'));
 
     }
 

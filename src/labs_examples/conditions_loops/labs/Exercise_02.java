43670c6db67eb205728ea617305dbbd7f8e37980
Matt <ammn2000@gmail.com>
2024-08-01 20:13:18 -0400

    Exc 2 loops/conditionals

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
  *
@@ -14,8 +16,25 @@ package labs_examples.conditions_loops.labs;
 public class Exercise_02 {
 
     public static void main(String[] args) {
-    
+        
+        Scanner scanner = new Scanner(System.in);
         // write completed here
+        int number = scanner.nextInt();
 
+        if(number == 1) {
+            System.out.println("Monday");
+        } else if(number == 2) {
+            System.out.println("Tuesday");
+        } else if(number == 3) {
+            System.out.println("Wednesday");
+        } else if(number == 4) {
+            System.out.println("Thursday");
+        } else if(number == 5) {
+            System.out.println("Friday");
+        } else if(number == 6) {
+            System.out.println("Saturday");
+        } else if(number == 7) {
+            System.out.println("Sunday");
+        }
     }
 }

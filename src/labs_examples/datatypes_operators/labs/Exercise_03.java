a4648f2415fb7f5f4cc2c4d0225e0321b1b4dc87
mterr026 <mterr026@fiu.edu>
2025-06-24 01:07:48 -0400

    datatype operators exercise 3 complete

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -11,7 +11,17 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        int addition = 1 + 2;
+        int subtraction = 2 - 1;
+        int multiplication = 2 * 4;
+        float division = 3.0f / 2.0f;
+        float modulus = 7f % 2f;
 
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+        System.out.println ("Modulus: " + modulus);
     }
 
 }

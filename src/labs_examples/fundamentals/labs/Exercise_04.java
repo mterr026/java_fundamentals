64fa946cbc5f7630fc75d59d66f50d9e12c779ab
mterr026 <mterr026@fiu.edu>
2025-06-23 20:57:15 -0400

    exercise 4 completed

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,7 +16,23 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        byte typeByte = 127;
+        short typeShort = 2048;
+        int typeInt = 1233124;
+        long typeLong = 1233582585;
+        float typeFloat = 1.02124f;
+        double typeDouble = 1.234322342;
+        boolean typeBoolean = false;
+        char typeChar = 'f';
 
+        System.out.println("Byte: " + typeByte);
+        System.out.println("Short: " + typeShort);
+        System.out.println("Int: " + typeInt);
+        System.out.println("Long: " + typeLong);
+        System.out.println("Float: " + typeFloat);
+        System.out.println("Double: " + typeDouble);
+        System.out.println("Boolean: " + typeBoolean);
+        System.out.println("Char: " + typeChar);
     }
 
 }

9eab5e28b6bfb0e0ddec360d9811e683e4c8fa9a
mterr026 <mterr026@fiu.edu>
2025-06-23 20:43:52 -0400

    exercise 3 completed

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -1,5 +1,6 @@
 package labs_examples.fundamentals.labs;
 
+
 /**
  * Fundamentals Exercise 3: My First Program
  *
@@ -17,7 +18,7 @@ public class Exercise_03 {
         System.out.println("Hello World!");
 
         // write code to print out "Check out my first Java program!" below
-
+        System.out.println("Check out my first Java program!");
     }
 
 }

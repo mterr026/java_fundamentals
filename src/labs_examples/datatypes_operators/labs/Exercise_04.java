d8ba7c17658fe09435315d49a7ad1dd382df1f94
mterr026 <mterr026@fiu.edu>
2025-06-24 01:14:12 -0400

    datatypes operators exercise 4 complete

--- src/labs_examples/datatypes_operators/labs/Exercise_04.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_04.java
@@ -13,12 +13,29 @@ class RelationalOperators {
         // example of "less than"
         int a = 1;
         int b = 2;
+        int c = 1;
+        int d = 1;
+
         if (a < b){
             System.out.println("a is less than b");
         }
 
         // write your code below
+        if (a <= b) {
+            System.out.println("a is less than or equal to b");
+        }
 
+        if (b > a) {
+            System.out.println("b is greater than a");
+        }
+
+        if (a >= b) {
+            System.out.println("b is greater than or equal to a");
+        }
+
+        if (c == d) {
+            System.out.println("c is equal to d");
+        }
     }
 
 }

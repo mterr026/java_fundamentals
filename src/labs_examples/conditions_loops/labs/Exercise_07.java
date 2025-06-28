88621819006df4250cd1fe29616e4c4d52cdba2d
mterr026 <mterr026@fiu.edu>
2025-06-28 16:49:52 -0400

    conditions_loops exercise 7 complete

--- src/labs_examples/conditions_loops/labs/Exercise_07.java
+++ src/labs_examples/conditions_loops/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 7: First vowel
  *
@@ -14,4 +16,21 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //take users input as a string
+        String word = scanner.nextLine();
+        String vowels = "aeiou";
+        //set the count to 0 for while loop
+        int count = 0;
+
+        while(count != word.length()) { //while count is less than the length of users word
+            int number = word.charAt(count); //convert the current character of users word to its ASCII value
+            if(vowels.indexOf(number) >= 0) { //check if current character is part of vowels string
+                System.out.println("The first vowel is: " + (char)number); //print out the first vowel in users word
+                break;
+            }
+            count++;
+        }
+    }
 }

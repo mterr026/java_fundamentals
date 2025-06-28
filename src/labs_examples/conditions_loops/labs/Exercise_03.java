9d271db87c1e61170741dfda68d7557812967ee7
mterr026 <mterr026@fiu.edu>
2025-06-27 21:17:17 -0400

    Conditions_loops exercise 3 complete

--- src/labs_examples/conditions_loops/labs/Exercise_03.java
+++ src/labs_examples/conditions_loops/labs/Exercise_03.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
+import org.w3c.dom.ls.LSOutput;
 
+import java.util.Scanner;
 /**
  * Conditions and Loops Exercise 3: Months of the year
  *
@@ -9,5 +11,43 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_03 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number between 1 and 12: ");
+        int number = scanner.nextInt();
+
+        switch(number) {
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+            case 3:
+                System.out.println("March");
+            case 4:
+                System.out.println("April");
+            case 5:
+                System.out.println("May");
+            case 6:
+                System.out.println("June");
+            case 7:
+                System.out.println("July");
+            case 8:
+                System.out.println("August");
+            case 9:
+                System.out.println("September");
+            case 10:
+                System.out.println("Obctober");
+            case 11:
+                System.out.println("November");
+            case 12:
+                System.out.println("December");
+
+        }
+    }
+
+
 
 }

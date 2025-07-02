3ac5d9f58d80557f7b3cf3dab91bb0362bab7e0e
mterr026 <mterr026@fiu.edu>
2025-07-02 18:53:13 -0400

    arrays labes exercise 2 complete

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  *  More labs_examples.arrays
  *
@@ -13,9 +15,16 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-
+        Scanner scanner = new Scanner(System.in);
         // write code here
+        System.out.println("Choose a number between 1 and 10 to see its index position in the array: ");
+        int number = scanner.nextInt();
 
+        for (int i = 0; i <= 9; i++) {
+            if(number == array[i]) {
+                System.out.println(number + " is at index: " + i);
+            }
+        }
 
     }
 }
